a8b6c4b4daea013d39327f7cc606165ca40b88d2
Syed-Muhammad-Ahmer <syedmuhammadahmernaqvi@gmail.com>
2025-05-06 00:28:42 +0500

    implemented Loading module

---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,12 @@
+import styles from './Loading.module.css';
+
+const Loading = () => {
+  return (
+    <div className={styles.loadingContainer}>
+      <div className={styles.loadingSpinner}></div>
+      <p>Loading...</p>
+    </div>
+  );
+};
+
+export default Loading;
