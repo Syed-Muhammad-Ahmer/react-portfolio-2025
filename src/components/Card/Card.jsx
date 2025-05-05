f1f75b034f3c9969dcaa4b539c6e1f968127d9bb
Syed-Muhammad-Ahmer <syedmuhammadahmernaqvi@gmail.com>
2025-05-06 00:33:10 +0500

    Implemented Card with basic functionalities like card content ,card footer

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,40 @@
+
+import styles from './Card.module.css';
+
+const Card = ({ 
+  title = '', 
+  description = '', 
+  technologies = [], 
+  image, 
+  link 
+}) => {
+  return (
+    <div className={styles.card}>
+      {image && (
+        <div className={styles.imageContainer}>
+          <img src={image} alt={title} className={styles.cardImage} />
+        </div>
+      )}
+      <div className={styles.cardContent}>
+        {title && <h3>{title}</h3>}
+        {description && <p>{description}</p>}
+        {technologies && technologies.length > 0 && (
+          <div className={styles.technologies}>
+            {technologies.map((tech, index) => (
+              <span key={index}>{tech}</span>
+            ))}
+          </div>
+        )}
+      </div>
+      {link && (
+        <div className={styles.cardFooter}>
+          <a href={link} target="_blank" rel="noopener noreferrer">
+            View Project
+          </a>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Card;
