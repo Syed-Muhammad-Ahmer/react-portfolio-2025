ee3f44085315e59b7084a01bf461fca53d05cf32
Syed-Muhammad-Ahmer <syedmuhammadahmernaqvi@gmail.com>
2025-05-06 01:01:53 +0500

    Enhanced Education.jsx

--- src/pages/Contact/Education/Education.jsx
+++ src/pages/Contact/Education/Education.jsx
@@ -1,1 +1,41 @@
+import { useContext } from 'react';
+import { usePageTitle } from '../../context/PageTitleContext';
+import CustomTable from '../../components/CustomTable/CustomTable';
+import styles from './Education.module.css';
 
+const Education = () => {
+  const { setTitle } = usePageTitle();
+  setTitle('Education');
+
+  const educationData = [
+    {
+      degree: 'FSC',
+      institution: 'Government College University, Lahore',
+      year: '2021-2023',
+      grade: '90%'
+    },
+    {
+      degree: 'Bachelors  in Computer Science',
+      institution: 'Information Technology University, Lahore',
+      year: '2023-2027',
+      grade: 'CGPA: 3.4'
+    },
+    {
+      degree: 'Web Development Bootcamp',
+      institution: 'Code Academy',
+      year: '2024',
+      grade: 'Certificate with Distinction'
+    }
+  ];
+
+  return (
+    <div className={styles.education}>
+      <h1>My Education</h1>
+      <div className={styles.tableWrapper}>
+        <CustomTable data={educationData} />
+      </div>
+    </div>
+  );
+};
+
+export default Education;
